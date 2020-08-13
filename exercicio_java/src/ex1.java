1fc515eec057e2c5f2b616909a4d9c7b2a5bb51c
brunoleonardomach <69649693+brunoleonardomach@users.noreply.github.com>
2020-08-13 16:57:48 -0300

    java

---
+++ exercicio_java/src/ex1.java
@@ -0,0 +1,21 @@
+
+public class ex1 {
+
+	public static void main(String[] args) {
+		
+		String project1 = "Computer";
+		String project2 = "Office desk";
+		int age = 30;
+		int code = 5290;
+		char gender = 'F';
+		double price1 = 2100.0;
+		double price2 = 650.50;
+		double measure = 53.234567;
+		System.out.printf("Products: %n" + "Computer, which price is $ %.2f" , price1);
+		System.out.printf("%nOffice desk, which price is $ %.2f", price2);
+		System.out.printf("%nRecord: years old, code %d and gender %s",age , code, gender);
+		
+		
+	}
+
+}
